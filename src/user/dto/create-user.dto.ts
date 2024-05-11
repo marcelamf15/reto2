fc413d83dbc006bb6e9e8d4e0e42ee159ac86e5b
marcela <florezmarcela1508@gmail.com>
2024-05-10 20:08:45 -0500

    clase validacion

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,18 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, MaxLength, MinLength, minLength } from "class-validator";
+
+export class CreateUserDto {
+    @IsEmail({},
+        {
+            message:'formato de email invalido'
+        })
+  email: string
+  @MinLength(10,{
+    message:'minimo 10 caracteres'
+  })
+  @MaxLength(10,{
+    message: 'Maximo de 10 caracteres'
+  })
+  firstName: string;
+  @IsNotEmpty()
+  lastName: string;
+}
